90393bcc6c019148ec16abf26dc242ff0576591d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-12 11:10:00 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,11 @@
-(() => {
-    console.log('hello')
+    console.log('hello');
 
     // WebsocketのURL
     const url = '';
     // Websocketの接続
     var socket = io.connect(url);
 
-})()
+    function sendMessage() {
+        var message = document.getElementById('message').value;
+        console.log(message);
+    }
